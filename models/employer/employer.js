5fde7c36926b66b6b83609f9dbee6f114ef0f951
kelvinmushy <74298025+kelvinmushy@users.noreply.github.com>
2024-12-15 16:23:11 +0300

    fix:industry_id

--- models/employer/employer.js
+++ models/employer/employer.js
@@ -39,9 +39,10 @@ const getEmployerById = async (id) => {
 const createEmployer = async (connection, user_id, otherDetails) => {
   try {
     const [result] = await connection.query(
-      'INSERT INTO employers (user_id, state_id, company_name, address, logo, phonenumber, employer_email, aboutCompany) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+      'INSERT INTO employers (user_id, industry_id, state_id, company_name, address, logo, phonenumber, employer_email, aboutCompany) VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)',
       [
         user_id,
+        otherDetails.industry_id,
         otherDetails.state_id,
         otherDetails.company_name,
         otherDetails.address,
